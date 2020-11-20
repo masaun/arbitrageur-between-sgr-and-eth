c152c39d7153d8e839e5cab3c6161f7db33898ce
masaun <masanoriuno@gmail.com>
2020-11-20 22:38:02 +0900

    Add 'ContractAddressLocatorProxy' into contractAddress.js

--- migrations/addressesList/contractAddress/contractAddress.js
+++ migrations/addressesList/contractAddress/contractAddress.js
@@ -7,7 +7,8 @@ module.exports = {
         "Sogur": {
             "SGRToken": "0xEd8F60f02c333d3B0E6Cf0553C19df7E9A99226F",
             "SGRAuthorizationManager": "0xd1F9c26bc7E66896A9ED1d8631dBA962Cc4e754d",
-            "AuthorizationDataSource": "0x9663Ed9c7690C4748C5418b549c55396035A82E5"
+            "AuthorizationDataSource": "0x9663Ed9c7690C4748C5418b549c55396035A82E5",
+            "ContractAddressLocatorProxy": "0xBAf0479a887c3027fC73202eeb8a88aD3B845e0b"
         },
     },
     "Kovan": {
