3281fef1e12f5b7a727c158f9b69da34758b84b6
masaun <masanoriuno@gmail.com>
2020-11-20 17:56:33 +0900

    Add ISGRAuthorizationManager.sol

---
+++ contracts/sogur/interfaces/ISGRAuthorizationManager.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.6.6;
+
+/**
+ * @title SGR Authorization Manager Interface.
+ */
+interface ISGRAuthorizationManager {
+    /**
+     * @dev Determine whether or not a user is authorized to buy SGR.
+     * @param _sender The address of the user.
+     * @return Authorization status.
+     */
+    function isAuthorizedToBuy(address _sender) external view returns (bool);
+
+    /**
+     * @dev Determine whether or not a user is authorized to sell SGR.
+     * @param _sender The address of the user.
+     * @return Authorization status.
+     */
+    function isAuthorizedToSell(address _sender) external view returns (bool);
+
+    /**
+     * @dev Determine whether or not a user is authorized to transfer SGR to another user.
+     * @param _sender The address of the source user.
+     * @param _target The address of the target user.
+     * @return Authorization status.
+     */
+    function isAuthorizedToTransfer(address _sender, address _target) external view returns (bool);
+
+    /**
+     * @dev Determine whether or not a user is authorized to transfer SGR from one user to another user.
+     * @param _sender The address of the custodian user.
+     * @param _source The address of the source user.
+     * @param _target The address of the target user.
+     * @return Authorization status.
+     */
+    function isAuthorizedToTransferFrom(address _sender, address _source, address _target) external view returns (bool);
+
+    /**
+     * @dev Determine whether or not a user is authorized for public operation.
+     * @param _sender The address of the user.
+     * @return Authorization status.
+     */
+    function isAuthorizedForPublicOperation(address _sender) external view returns (bool);
+}
